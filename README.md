3e161e811bef3a5033a24e5d0b669ddbbda48023
hristo-grudev <hr.grudev@gmail.com>
2021-04-21 09:57:03 +0300

    Job is done

---
+++ README.md
@@ -0,0 +1,8 @@
+URL: https://www.adcb.com/en/about-us/media-centre/news/newsjson.json
+
+Spider name: adcb
+DB Schema:
+
+title
+content
+date
---
+++ adcb/settings.py
@@ -0,0 +1,16 @@
+BOT_NAME = 'adcb'
+
+SPIDER_MODULES = ['adcb.spiders']
+NEWSPIDER_MODULE = 'adcb.spiders'
+FEED_EXPORT_ENCODING = 'utf-8'
+LOG_LEVEL = 'ERROR'
+DOWNLOAD_DELAY = 0
+
+ROBOTSTXT_OBEY = True
+
+ITEM_PIPELINES = {
+	'adcb.pipelines.AdcbPipeline': 100,
+
+}
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
